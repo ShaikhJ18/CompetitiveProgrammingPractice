981eea7c692317f4495fac6e1d3bd706891abcf6
ShaikhJ18 <J.shk1980@gmail.com>
2025-01-17 22:10:16 -0500

    learned basic string input in c++

---
+++ chapter1/inputoutput/basicStringInput.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    //store the sentence
+    string sentence;
+    //get the whole sentence from the command line
+    getline(cin,sentence);
+
+    cout << sentence;
+    //output the string to the command line
+
+    return 0;
+}
