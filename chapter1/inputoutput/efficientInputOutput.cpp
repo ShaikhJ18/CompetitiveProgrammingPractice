d2b49d40f1820a02c111161fa8f1c6b7bd8b66e8
ShaikhJ18 <J.shk1980@gmail.com>
2025-01-17 22:04:01 -0500

    more efficient ways to get io

---
+++ chapter1/inputoutput/efficientInputOutput.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+    //this statement makes io faster
+    ios::sync_with_stdio(0);
+    //this statement makes io faster
+    cin.tie(0);
+
+    int a,b;
+    //using the functions from c to make io efficient
+    scanf("%d %d", &a, &b);
+    printf("%d %d", a, b);
+
+    return 0;
+}
