ce22dc8b60bebeea4d23a3d2ce8d8dea7e413106
ShaikhJ18 <J.shk1980@gmail.com>
2025-01-17 21:57:13 -0500

    learning basic io for c++

---
+++ chapter1/inputoutput/basicInputOutput.cpp
@@ -0,0 +1,15 @@
+//include the library for common data structures and algorithms and standard i/o
+#include<bits/stdc++.h>
+//using the standard namespace to easily do input and output
+using namespace std;
+int main(){
+
+    //integers a and b
+    int a,b;
+    //get input from the user
+    cin >> a >> b;
+    //output two integers entered by the user
+    cout << a << b;
+
+    return 0;
+}
